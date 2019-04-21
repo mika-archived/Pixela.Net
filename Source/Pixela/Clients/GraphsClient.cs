8550f8cc02f97ffeb86cdc535db64c06b5c81767
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-04-21 23:11:56 +0900

    feat: support Pixela 1.8, 1.9 and 1.10 features
    * Add a `/stats`
    * Add a `/pixels`

--- Source/Pixela/Clients/GraphsClient.cs
+++ Source/Pixela/Clients/GraphsClient.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 using Newtonsoft.Json;
 
+using Pixela.Converters;
 using Pixela.Enums;
 using Pixela.Extensions;
 using Pixela.Models;
@@ -25,7 +26,10 @@ namespace Pixela.Clients
         /// <param name="type">type of quantity</param>
         /// <param name="color">display color of the pixel</param>
         /// <param name="timezone">timezone for handling this graph, default UTC</param>
-        /// <param name="selfSufficient">when SVG graph referenced, pixel of this graph itself will be increment or decrement, default none</param>
+        /// <param name="selfSufficient">
+        ///     when SVG graph referenced, pixel of this graph itself will be increment or decrement,
+        ///     default none
+        /// </param>
         /// <returns></returns>
         public async Task<ApiResponse> CreateAsync(string id, string name, string unit, GraphType type, GraphColor color, string timezone = null, SufficientType? selfSufficient = null)
         {
@@ -39,7 +43,7 @@ namespace Pixela.Clients
             };
             if (!string.IsNullOrWhiteSpace(timezone))
                 parameters.Add("timezone", timezone);
-            if(selfSufficient.HasValue)
+            if (selfSufficient.HasValue)
                 parameters.Add("selfSufficient", selfSufficient.Value.AsString());
 
             return await Client.SendAsync<ApiResponse>(HttpMethod.Post, $"/v1/users/{Client.Username}/graphs", parameters).Stay();
@@ -62,7 +66,7 @@ namespace Pixela.Clients
         /// <param name="date">If specify date, will create a graph dating back to the past with that day as the start date</param>
         /// <param name="mode">Graph display mode</param>
         /// <returns></returns>
-        public async Task<Stream> ShowAsync(string graphId, DateTime? date, string mode = null)
+        public async Task<string> ShowAsync(string graphId, DateTime? date = null, string mode = null)
         {
             var parameters = new Dictionary<string, object>();
             if (date.HasValue)
@@ -70,7 +74,7 @@ namespace Pixela.Clients
             if (!string.IsNullOrWhiteSpace(mode))
                 parameters.Add("mode", mode);
 
-            return await Client.GetAsStreamAsync($"/v1/users/{Client.Username}/graphs/{graphId}", parameters).Stay();
+            return await Client.GetAsync($"/v1/users/{Client.Username}/graphs/{graphId}", parameters).Stay();
         }
 
         /// <summary>
@@ -82,7 +86,10 @@ namespace Pixela.Clients
         /// <param name="color">display color of the pixel</param>
         /// <param name="timezone">timezone for handling this graph</param>
         /// <param name="purgeCacheUrls"></param>
-        /// <param name="selfSufficient">when SVG graph referenced, pixel of this graph itself will be increment or decrement, default none</param>
+        /// <param name="selfSufficient">
+        ///     when SVG graph referenced, pixel of this graph itself will be increment or decrement,
+        ///     default none
+        /// </param>
         /// <returns></returns>
         public async Task<ApiResponse> UpdateAsync(string graphId, string name = null, string unit = null, GraphColor? color = null, string timezone = null, List<string> purgeCacheUrls = null, SufficientType? selfSufficient = null)
         {
@@ -116,11 +123,60 @@ namespace Pixela.Clients
         /// <summary>
         ///     Displays the details of the graph in html format.
         /// </summary>
-        /// <param name="graphId">UD of graph</param>
+        /// <param name="graphId">ID of graph</param>
         /// <returns></returns>
         public async Task<string> DetailsAsync(string graphId)
         {
             return await Task.FromResult($"https://pixe.la/v1/users/{Client.Username}/graphs/{graphId}.html").Stay();
+        }
+
+        /// <summary>
+        ///     Get a Date list of Pixel registered in the graph specified by graphID.
+        ///     You can specify a period with from and to parameters.
+        ///     <list type="bullet">
+        ///         <item>
+        ///             <description>If you do not specify both `from` and `to`, you will get a list of 365 days ago from today.</description>
+        ///         </item>
+        ///         <item>
+        ///             <description>If you specify `from` only, you will get a list of 365 days from `from` date.</description>
+        ///         </item>
+        ///         <item>
+        ///             <description>If you specify `to` only, you will get a list of 365 days ago from `to` date.</description>
+        ///         </item>
+        ///         <item>
+        ///             <description>
+        ///                 If you specify both `from` and `to`, you will get a list you specify.
+        ///                 You can not get specify a period greater than 365 days.
+        ///             </description>
+        ///         </item>
+        ///     </list>
+        /// </summary>
+        /// <param name="graphId">ID of the graph</param>
+        /// <param name="from">Specify the start position of the period.</param>
+        /// <param name="to">Specify the end position of the period.</param>
+        /// <returns></returns>
+        public async Task<List<DateTime>> PixelsAsync(string graphId, DateTime? from = null, DateTime? to = null)
+        {
+            var parameters = new Dictionary<string, object>();
+            if (from.HasValue)
+                parameters.Add("from", from.Value.ToString("yyyyMMdd"));
+            if (to.HasValue)
+                parameters.Add("to", to.Value.ToString("yyyyMMdd"));
+
+            var response = await Client.GetAsync<ApiResponse>($"/v1/users/{Client.Username}/graphs/{graphId}/pixels", parameters).Stay();
+            var serializer = new JsonSerializer();
+            serializer.Converters.Add(new StringToDateTimeConverter());
+            return response.Extends["pixels"].Select(w => w.ToObject<DateTime>(serializer)).ToList();
+        }
+
+        /// <summary>
+        ///     Based on the registered information, get various statistics.
+        /// </summary>
+        /// <param name="graphId">ID of graph</param>
+        /// <returns></returns>
+        public async Task<GraphStats> StatsAsync(string graphId)
+        {
+            return await Client.GetAsync<GraphStats>($"/v1/users/{Client.Username}/graphs/{graphId}/stats").Stay();
         }
     }
 }
---
+++ Source/Pixela/Converters/StringToDateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace Pixela.Converters
+{
+    internal class StringToDateTimeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+                return DateTime.ParseExact(reader.Value.ToString(), "yyyyMMdd", null);
+            throw new NotSupportedException();
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+    }
+}
